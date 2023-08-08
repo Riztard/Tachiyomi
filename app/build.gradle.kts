de418d22b1885103815a9ad9d70436acaea7fdff
Riztard Lanthorn <riyanluqman@gmail.com>
2023-08-08 14:04:26 +0700

    Build Riz+

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
     namespace = "eu.kanade.tachiyomi"
 
     defaultConfig {
-        applicationId = "eu.kanade.tachiyomi.sy"
+        applicationId = "eu.kanade.tachiyomi.sy.riztard"
 
         versionCode = 60
         versionName = "1.10.0"
@@ -51,7 +51,7 @@ android {
 
     buildTypes {
         named("debug") {
-            versionNameSuffix = "-${getCommitCount()}"
+            versionNameSuffix = "-${getCommitCount()}-riz"
             applicationIdSuffix = ".debug"
             isPseudoLocalesEnabled = true
         }
@@ -63,6 +63,7 @@ android {
             matchingFallbacks.add("release")
         }
         named("release") {
+            versionNameSuffix = "-riz"
             isMinifyEnabled = true
             isShrinkResources = true
             setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
