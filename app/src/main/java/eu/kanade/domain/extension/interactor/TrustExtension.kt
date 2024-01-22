9461a06c2c705a74063cb13a6fe07bf6be00ed19
Riztard <16263232+Riztard@users.noreply.github.com>
2024-01-22 16:57:15 +0700

    ⚠Trust any Ext by default⚠

--- app/src/main/java/eu/kanade/domain/extension/interactor/TrustExtension.kt
+++ app/src/main/java/eu/kanade/domain/extension/interactor/TrustExtension.kt
@@ -4,12 +4,17 @@ import android.content.pm.PackageInfo
 import androidx.core.content.pm.PackageInfoCompat
 import eu.kanade.domain.source.service.SourcePreferences
 import tachiyomi.core.common.preference.getAndSet
+import tachiyomi.domain.UnsortedPreferences
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
 class TrustExtension(
     private val preferences: SourcePreferences,
 ) {
+    val unsortedPreferences = Injekt.get<UnsortedPreferences>()
 
     fun isTrusted(pkgInfo: PackageInfo, signatureHash: String): Boolean {
+        if (unsortedPreferences.superSecretSetting().get() > 2) return true
         val key = "${pkgInfo.packageName}:${PackageInfoCompat.getLongVersionCode(pkgInfo)}:$signatureHash"
         return key in preferences.trustedExtensions().get()
     }
