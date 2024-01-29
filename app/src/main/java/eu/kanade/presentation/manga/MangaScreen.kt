e34fbff870c09a8ed37bd0a694ee84ca5fb8bde7
Riztard <16263232+Riztard@users.noreply.github.com>
2024-01-29 08:43:43 +0700

    Fix FAB decimal probably

--- app/src/main/java/eu/kanade/presentation/manga/MangaScreen.kt
+++ app/src/main/java/eu/kanade/presentation/manga/MangaScreen.kt
@@ -422,10 +422,10 @@ private fun MangaScreenSmallImpl(
                         val isReading = remember(state.chapters) {
                             state.chapters.fastAny { it.chapter.read }
                         }
-                        var chapterNumber = state.chapters.getNextUnread(state.manga)?.chapterNumber.toString()
+                        var chapterNumber = state.chapters.getNextUnread(state.manga)?.chapterNumber!!.toFloat().toString()
                             .replace(".0", "")
-                            .substringBefore("00000")
-                            .replace("9999", ".1x").substringBefore("x")
+//                            .substringBefore("00000")
+//                            .replace("9999", ".1x").substringBefore("x")
                         Text(
                             text = stringResource(if (isReading) MR.strings.action_resume else MR.strings.action_start) + " Ch " + chapterNumber
                         )
